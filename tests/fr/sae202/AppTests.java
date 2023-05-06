0284196a3d1a3d81473c1785609b6f3aa652388b
NoXeDev <noyonne2@gmail.com>
2023-05-06 12:08:04 +0200

    change test API (Junit4 -> Jupiter Junit)

--- tests/fr/sae202/AppTests.java
+++ tests/fr/sae202/AppTests.java
@@ -1,7 +1,7 @@
 package fr.sae202;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTests {
     @Test public void appHasAGreeting() {
