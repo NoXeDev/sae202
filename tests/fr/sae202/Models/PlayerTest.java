3003338de15054c8267e7640406fa6bb4dcdd7c5
NoXeDev <noyonne2@gmail.com>
2023-06-04 13:22:55 +0200

    Algorithms Test

--- tests/fr/sae202/Models/PlayerTest.java
+++ tests/fr/sae202/Models/PlayerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
